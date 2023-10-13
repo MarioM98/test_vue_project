39f0fd6b7e642848b0030e8a2291cc2f4ff57e43
MarioM98 <mariomattana1298@gmail.com8>
2023-10-13 10:20:03 +0200

    First Commit

---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <ButtonSection @btn-click="$emit('toggle-add-task')" 
+        :text="showAddTask ? 'Close' : 'Add Task'" 
+        :color="showAddTask ? 'red' : 'green'"/>
+    </header>
+</template>
+
+<script>
+
+import ButtonSection from './ButtonSection.vue';
+
+export default {
+    name: 'HeaderSection',
+    props: {
+        title: String,
+        showAddTask: Boolean
+    },
+    components: {
+        ButtonSection
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
---
+++ src/components/TasksGest.vue
@@ -0,0 +1,22 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+        <SingleTaskGest @toggle-reminder="$emit('toggle-reminder', task.id)" 
+        @delete-task="$emit('delete-task', task.id)" :task="task"></SingleTaskGest>
+    </div>
+</template>
+
+<script>
+
+import SingleTaskGest from './SingleTaskGest'
+
+export default {
+    name: 'TasksGest',
+    props: {
+        tasks: Array,
+    },
+    components: {
+        SingleTaskGest,
+    },
+    emits: ['delete-tasks','toggle-reminder'],
+}
+</script>
